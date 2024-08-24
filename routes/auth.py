60e468213d72f6f7d34ab50aaf1491fbb02f49b7
Eliot Alderson <arnavghosh67@gmail.com>
2024-08-24 20:16:38 +0530

    Expiry

--- routes/auth.py
+++ routes/auth.py
@@ -56,7 +56,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=5)
+    access_token_expires = timedelta(minutes=int(os.getenv('JWT_DURATION', 5)))
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
